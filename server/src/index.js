94907e7aef3e1914ea34c9a4241fb57c7755d8b0
Mateus Henrique <mateusdzn10@gmail.com>
2022-06-11 16:20:36 -0400

    initialize socket chat

---
+++ server/src/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../../client')));
+
+app.get('/chats', (req, res) => {
+	res.send('My chat ');
+});
+const server = app.listen(process.env.PORT, () => console.log());
